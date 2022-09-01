faf44bbc38da499a7b59e19707bb6525f5f23236
Lucas <lucas.tallos@tallos.com.br>
2022-09-01 16:41:25 -0300

    feat: teste

--- src/movie/entities/movie.entity.ts
+++ src/movie/entities/movie.entity.ts
@@ -1,7 +1,7 @@
-import { Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
 @Schema()
 export class Movie {
-    
+
 }
 export const MovieSchema = SchemaFactory.createForClass(Movie);
