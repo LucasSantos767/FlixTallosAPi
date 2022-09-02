b3476689d855dc94c7b2900cc6db9f460114a248
Lucas <lucas.tallos@tallos.com.br>
2022-09-02 14:10:02 -0300

    feat: Criando rota de listar teatros com limit

--- src/theaters/entities/theater.entity.ts
+++ src/theaters/entities/theater.entity.ts
@@ -1,1 +1,4 @@
+import { Schema, SchemaFactory } from "@nestjs/mongoose";
+@Schema()
 export class Theater {}
+export const TheaterSchema = SchemaFactory.createForClass(Theater);
--- src/theaters/theaters.service.ts
+++ src/theaters/theaters.service.ts
@@ -1,15 +1,19 @@
+import { Model } from 'mongoose';
+import { Theater } from './entities/theater.entity';
 import { Injectable } from '@nestjs/common';
 import { CreateTheaterDto } from './dto/create-theater.dto';
 import { UpdateTheaterDto } from './dto/update-theater.dto';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class TheatersService {
+  constructor(@InjectModel(Theater.name) private TheaterModel: Model<Theater>) { }
   create(createTheaterDto: CreateTheaterDto) {
     return 'This action adds a new theater';
   }
 
   findAll() {
-    return `This action returns all theaters`;
+    return this.TheaterModel.find().limit(6);
   }
 
   findOne(id: number) {
