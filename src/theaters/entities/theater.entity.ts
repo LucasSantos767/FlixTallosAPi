54549afd9e952b2bfd7458d2d519b8b520c388cb
Lucas <lucas.tallos@tallos.com.br>
2022-09-02 15:07:00 -0300

    feat: Criando schema dos teatros e adicionando interface

--- src/theaters/entities/theater.entity.ts
+++ src/theaters/entities/theater.entity.ts
@@ -1,4 +1,10 @@
-import { Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import ITheater from "../interfaces/ITheater";
 @Schema()
-export class Theater {}
+export class Theater {
+    @Prop()
+    theaterid: number;
+    @Prop({ type: Object })
+    location: ITheater
+}
 export const TheaterSchema = SchemaFactory.createForClass(Theater);
