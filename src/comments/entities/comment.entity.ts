7372a101b028dd5434f1f4dd99ce382af5841849
Lucas <lucas.tallos@tallos.com.br>
2022-09-05 10:56:00 -0300

    feat: Criando entity

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,1 +1,17 @@
-export class Comment {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ObjectId } from "mongoose";
+
+@Schema({ versionKey: false })
+export class Comment {
+    @Prop()
+    name: string;
+    @Prop()
+    email: string;
+    @Prop()
+    movie_id: ObjectId
+    @Prop()
+    text: string;
+    @Prop()
+    date: Date
+}
+export const CommentSchema = SchemaFactory.createForClass(Comment);
