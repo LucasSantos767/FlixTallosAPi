9ed82fc37642cf5aee91263d9dc75f347d40f260
Lucas <lucas.tallos@tallos.com.br>
2022-09-05 15:15:10 -0300

    feat: Adicionando guards no app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,10 +9,14 @@ import { MovieModule } from './movie/movie.module';
 import { TheatersModule } from './theaters/theaters.module';
 import { SessionsModule } from './sessions/sessions.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.DB_CONNECT),AuthModule, UsersModule, CommentsModule, MovieModule, TheatersModule, SessionsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,{
+    provide:APP_GUARD,
+    useClass: JwtAuthGuard
+  }],
 })
 export class AppModule { }
